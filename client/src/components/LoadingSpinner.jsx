a98987c137e43213f3eec7a4e1c08f2f4202d928
Shimara-Appuhami <shimarailshani@gmail.com>
2025-11-24 21:27:59 +0530

    add LoadingSpinner

---
+++ client/src/components/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+const LoadingSpinner = () => (
+  <div className="flex h-full min-h-[220px] items-center justify-center">
+    <span className="loader" role="status" aria-label="Loading" />
+  </div>
+);
+
+export default LoadingSpinner;
